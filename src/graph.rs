bdfafeab74265412131b4d07bff45e5e8d9f7760
michirakara <michirakara@gmail.com>
2024-09-29 10:54:45 -0700

    refactor

--- src/graph.rs
+++ src/graph.rs
@@ -1,5 +1,3 @@
-use std::iter::zip;
-
 #[derive(Clone, Debug)]
 enum Dfs<Idx> {
     Pre(Idx),
@@ -17,6 +15,14 @@ pub trait Graph {
         self.len() == 0
     }
 
+    /// 与えられた関数でDFSを実行する
+    ///
+    /// - `init` は `(index, data)` のタプル
+    /// - `pre_order(&idx, &nidx, &cost, &data) -> Option<data>` で行きがけの処理
+    ///     - 返り値が `Some(data)` なら `nidx` に対しても探索を行う
+    ///     - 返り値が `None` なら `nidx` に対しては探索は行わない
+    ///     - `dfs` 側では既に探索したかの判定は **行わない** ので `pre_order` 内で判定をすること
+    /// - `post_order(&idx, &data)` で帰りがけの処理
     fn dfs<D: Clone, F, G>(&self, init: (Self::Idx, D), mut pre_order: F, mut post_order: G)
     where
         F: FnMut(&Self::Idx, &Self::Idx, &Self::Cost, &D) -> Option<D>,
@@ -44,7 +50,7 @@ pub trait Graph {
         }
     }
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct UnweightedGraph {
     pub n: usize,
     pub g: Vec<Vec<usize>>,
@@ -62,7 +68,7 @@ impl Graph for UnweightedGraph {
         self.g[i].iter().map(|&i| (i, 1)).collect()
     }
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct WeightedGraph<Cost> {
     pub n: usize,
     pub g: Vec<Vec<(usize, Cost)>>,
@@ -80,7 +86,7 @@ impl<Cost: Clone> Graph for WeightedGraph<Cost> {
         self.g[i].clone()
     }
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Grid {
     pub is_8way: bool,
     pub h: usize,
@@ -100,38 +106,26 @@ impl Graph for Grid {
         if !self.g[i.0][i.1] {
             return Vec::new();
         }
-        if self.is_8way {
-            const DX: [i32; 8] = [-1, -1, -1, 0, 0, 1, 1, 1];
-            const DY: [i32; 8] = [-1, 0, 1, -1, 1, -1, 0, 1];
-            let mut ret = vec![];
-            for (dx, dy) in zip(DX, DY) {
-                let nx: Option<usize> = ((i.0 as i32) + dx).try_into().ok();
-                let ny: Option<usize> = ((i.1 as i32) + dy).try_into().ok();
-                if let Some(nx) = nx {
-                    if let Some(ny) = ny {
-                        if nx < self.h && ny < self.w && self.g[nx][ny] {
-                            ret.push(((nx, ny), 1));
-                        }
-                    }
-                }
-            }
-            ret
+
+        let (dx, dy) = if self.is_8way {
+            (
+                vec![-1, -1, -1, 0, 0, 1, 1, 1],
+                vec![-1, 0, 1, -1, 1, -1, 0, 1],
+            )
         } else {
-            const DX: [i32; 4] = [-1, 0, 0, 1];
-            const DY: [i32; 4] = [0, -1, 1, 0];
-            let mut ret = vec![];
-            for (dx, dy) in zip(DX, DY) {
-                let nx: Option<usize> = ((i.0 as i32) + dx).try_into().ok();
-                let ny: Option<usize> = ((i.1 as i32) + dy).try_into().ok();
-                if let Some(nx) = nx {
-                    if let Some(ny) = ny {
-                        if nx < self.h && ny < self.w && self.g[nx][ny] {
-                            ret.push(((nx, ny), 1));
-                        }
-                    }
+            (vec![-1, 0, 0, 1], vec![0, -1, 1, 0])
+        };
+
+        let mut ret = vec![];
+        for (&dx, &dy) in dx.iter().zip(dy.iter()) {
+            let nx: Option<usize> = ((i.0 as i32) + dx).try_into().ok();
+            let ny: Option<usize> = ((i.1 as i32) + dy).try_into().ok();
+            if let (Some(nx), Some(ny)) = (nx, ny) {
+                if nx < self.h && ny < self.w && self.g[nx][ny] {
+                    ret.push(((nx, ny), 1));
                 }
             }
-            ret
         }
+        ret
     }
 }
